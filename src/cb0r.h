76cef82859fd2831c61bf083b5785dd11ca3def1
Jeremie Miller <jeremie@jabber.org>
2016-12-24 09:13:37 -0700

    prepping for reorg

--- src/cb0r.h
+++ src/cb0r.h
@@ -2,19 +2,25 @@
 #include <stdlib.h>
 
 // these are the possible types
-#define CB0R_ERR    0
-#define CB0R_INT    1   // (int64_t)val
-#define CB0R_UINT   2   // val
-#define CB0R_BYTE   3   // start+val
-#define CB0R_TEXT   4   // start+val
-#define CB0R_ARRAY  5   // val is items, start == first
-#define CB0R_MAP    6   // val is pairs, start == first
-#define CB0R_TAG    7   // val
-#define CB0R_FLOAT  8   // (double)val
-#define CB0R_TRUE   9   // n/a
-#define CB0R_FALSE  10  // n/a
-#define CB0R_NULL   11  // n/a
-#define CB0R_UNDEF  12  // n/a
-#define CB0R_END    13  // n/a
+enum {
+  CB0R_ERR = 0,
+  CB0R_INT  , // (int64_t)val
+  CB0R_UINT , // val
+  CB0R_BYTE , // start+val
+  CB0R_TEXT , // start+val
+  CB0R_ARRAY, // val is items, start == first
+  CB0R_MAP  , // val is pairs, start == first
+  CB0R_TAG  , // val
+  CB0R_FLOAT, // (double)val
+  CB0R_TRUE , // n/a
+  CB0R_FALSE, // n/a
+  CB0R_NULL , // n/a
+  CB0R_UNDEF, // n/a
+  CB0R_END
+}
 
 size_t cb0r(uint8_t *raw, size_t len, uint8_t *type, uint64_t *val, size_t *start);
+
+#define CB0R_CODE // table and labels
+#define CB0R_NEXT // sets type, start*, end*
+#define CB0R_VALUE() // uses type/start/end, fills in given value
